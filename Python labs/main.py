fd699e4a0d3386013ef86c3b597d47230824d681
Ismail910 <ismailmagdy566@gmail.com>
2023-03-19 02:11:51 +0200

    Python OOP

---
+++ Python labs/main.py
@@ -0,0 +1,14 @@
+from person import Person
+from Employee import Employee
+from office import Office
+
+employees = []
+employees.append(Employee("ahmed",10000,10,200))
+employees.append(Employee("joe",10000,10,200))
+
+
+of1=Office("it",employees)
+print(of1.get_All_employees())
+
+
+
